7de7a4f4c1045a9b40ffdd67865e745986f23697
LenaAbrt <olenaabert@Olenas-MacBook-Pro.local>
2025-07-17 16:29:13 -0400

    build API

---
+++ backend/controllers/product.controller.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+import Product from "../models/product.model.js";
+
+export const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find({}); //mongoose Чтение (поиск)
+    res.status(200).json({ succes: true, data: products });
+  } catch (error) {
+    console.log("Error in fetching products", error.message);
+    res.sendStatus(500).json({ succes: false, message: "Server Error" });
+  }
+};
+
+export const createProduct = async (req, res) => {
+  const product = req.body; //user will send this data
+
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ succes: false, message: "Please provide all field" });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save(); //mongoose method .save() to Создание и сохранение документа
+    res.status(201).json({ succes: true, data: newProduct });
+  } catch (error) {
+    console.log("Error in Create product", error.message);
+    res.status(500).json({ succes: false, message: "Server Error" });
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  const { id } = req.params;
+
+  const product = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ succes: false, message: "Invalid Product Id" });
+  }
+  try {
+    //.findByIdAndUpdate() Mongoose используется для обновления документа по его _id
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
+      new: true,
+    });
+    res.status(200).json({ succes: true, data: updatedProduct });
+  } catch (error) {
+    res.status(500).json({ succes: false, message: "Server Error" });
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ succes: true, message: "Product deleted" });
+  } catch (error) {
+    console.log("Can`t find this product to delete", error.message);
+    res.status(404).json({ succes: false, message: "Product not found" });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 
+import productRoutes from "./routes/product.route.js";
+
 dotenv.config();
 
 const app = express();
 
-app.get("/products", (req, res) => {});
+app.use(express.json()); //allows us to accept JSON data in the req.body
 
-// console.log(process.env.MONGO_URL);
+app.use("/api/products", productRoutes); //routes template link
 
 app.listen(5000, () => {
   connectDB();
